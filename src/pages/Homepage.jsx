3695239fc1094de3d5ec7bbbab8e3f74b7f17088
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-11 16:17:45 +0100

    change homepage for consistency with react app

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,35 +1,25 @@
 import EventList from "../components/events/EventList";
-import { useNavigate } from "react-router-dom";
-import EventsSearch from "../components/events/EventSearch";
 import { useEffect, useState } from "react";
 import styles from "./Homepage.module.css";
 import { getFeaturedEvents } from "../helpers/api";
 
 export default function Homepage() {
-  const navigate = useNavigate();
-  function findEventsHandler(year, month) {
-    const fullPath = `/events/${year}/${month}`;
-
-    navigate(fullPath);
-  }
-
-  const [events, setEvents] = useState([]);
+  const [getFeaturedEvents, setFeaturedEvents] = useState([]);
 
   useEffect(() => {
     getFeaturedEvents().then((events) => {
-      setEvents(events);
+      setFeaturedEvents(events);
     });
   }, []);
 
-  if (!events.length) {
+  if (!getFeaturedEvents.length) {
     return <p>Loading...</p>;
   }
 
   return (
     <div className="Homepage">
       <h1 className={styles.main_title}>Featured Events</h1>
-      <EventsSearch onSearch={findEventsHandler} />
-      <EventList events={events} />
+      <EventList events={getFeaturedEvents} />
     </div>
   );
 }
