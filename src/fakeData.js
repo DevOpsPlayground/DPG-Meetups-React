8565b7f4d51f3d63637b61f4e20907bacabfed41
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-15 15:29:17 +0100

    add changes

---
+++ src/fakeData.js
@@ -0,0 +1,29 @@
+export const fakeData = [
+  {
+    date: "Some day ",
+    featured: true,
+    id: 1,
+    location: "GL London Office",
+    presenters: ["Some GL Person"],
+    title: "Imported Data Fake Event 1",
+    description: "some description",
+  },
+  {
+    date: "Another day",
+    featured: true,
+    id: 2,
+    location: "GL London Office",
+    presenters: ["GL Delivery Consultant"],
+    title: "Imported Data Fake Event 2",
+    description: "some description",
+  },
+  {
+    date: "Maybe one day",
+    featured: true,
+    id: 3,
+    location: "GL Manchester Office",
+    presenters: ["Some GL Consultant"],
+    title: "Imported Data Fake Event 3",
+    description: "some description",
+  },
+];
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,34 +2,39 @@ import EventList from "../components/events/EventList";
 import { useEffect, useState } from "react";
 import styles from "./Homepage.module.css";
 import { getFeaturedEvents } from "../helpers/api";
+import { fakeData } from "../fakeData"; // imported fake data
 
 export default function Homepage() {
   const [featuredEvents, setFeaturedEvents] = useState([]);
 
   useEffect(() => {
-    getFeaturedEvents().then((events) => {
-      setFeaturedEvents(events);
-    });
+    /* IMPORTED fake data */
+    setFeaturedEvents(fakeData);
+
+    /* FETCHED real data */
+    // getFeaturedEvents().then((events) => {
+    //   setFeaturedEvents(events);
+    // });
   }, []);
 
   if (!featuredEvents.length) {
     return (
-      <div className="loading_container">
-        <div className="lds-ring">
+      <div className='loading_container'>
+        <div className='lds-ring'>
           {" "}
           <div></div>
           <div></div>
           <div></div>
           <div></div>
         </div>
-        <p className="loading_text">Getting your events...</p>
+        <p className='loading_text'>Getting your events...</p>
       </div>
     );
   }
 
   return (
-    <div className="Homepage">
-      <h1 className="main_title">Welcome to DevOps Playground Events Page</h1>
+    <div className='Homepage'>
+      <h1 className='main_title'>Welcome to DevOps Playground Events Page</h1>
       <div className={styles.feature_title_container}>
         <h2 className={styles.feature_title}>Featured Events</h2>
       </div>
